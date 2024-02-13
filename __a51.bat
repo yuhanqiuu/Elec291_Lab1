2edc01614aff88473d924921950f246c7617633c
yuhanqiuu <qiuyuhan66@gmail.com>
2024-02-13 08:47:56 -0800

    k

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\Lets_TRY.asm"
-echo Crosside_Action Set_Hex_File C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\Lets_TRY.HEX
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\PWM_demo.asm"
+echo Crosside_Action Set_Hex_File C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\PWM_demo.HEX
