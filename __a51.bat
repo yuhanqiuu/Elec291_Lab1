ef3a218b128e1048ef9fb92ac55117de16191316
yuhanqiuu <qiuyuhan66@gmail.com>
2024-02-14 12:25:05 -0800

    d

--- __a51.bat
+++ __a51.bat
@@ -1,5 +1,5 @@
 @echo off
 C:
 cd "\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\special_char.asm"
-echo Crosside_Action Set_Hex_File C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\special_char.HEX
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\Main.asm"
+echo Crosside_Action Set_Hex_File C:\Users\qiuyu\OneDrive\Documents\GitHub\Elec291_Lab1\Main.HEX
